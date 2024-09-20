a08ebf2592633f14461c6d4b918b85454199016c
sammar <sammaralaa95@gmail.com>
2024-09-20 22:20:37 +0300

    implement HomeFragmentViewModelFactory

---
+++ app/src/main/java/com/example/weatherproject/HomeFragmentViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.weatherproject
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherproject.model.WeatherRepository
+
+class HomeFragmentViewModelFactory(private val repo: WeatherRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeFragmentViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HomeFragmentViewModel(repo) as T
+        }else
+        {throw IllegalArgumentException("Unknown ViewModel class")}
+    }
+}
