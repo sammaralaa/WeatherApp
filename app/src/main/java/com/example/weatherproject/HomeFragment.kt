2e5900fab59373bb0cac5d985227f99ad871a756
sammar <sammaralaa95@gmail.com>
2024-09-20 11:55:23 +0300

    modify appBar

--- app/src/main/java/com/example/weatherproject/HomeFragment.kt
+++ app/src/main/java/com/example/weatherproject/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
 
 class HomeFragment : Fragment() {
 
