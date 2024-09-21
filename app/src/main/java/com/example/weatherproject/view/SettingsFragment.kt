fbe1ff9a1853ed5df064a1bf99ca6ebfe2e6538e
sammar <sammaralaa95@gmail.com>
2024-09-21 15:03:01 +0300

    choose location from map on settings fragment

--- app/src/main/java/com/example/weatherproject/view/SettingsFragment.kt
+++ app/src/main/java/com/example/weatherproject/view/SettingsFragment.kt
@@ -5,23 +5,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.navigation.fragment.findNavController
 import com.example.weatherproject.R
+import com.example.weatherproject.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
 
+    lateinit var locatioGroup : RadioGroup
+    lateinit var languageGroup : RadioGroup
+
+    lateinit var gpsButton : RadioButton
+    lateinit var mapButton : RadioButton
+    lateinit var arabicButton : RadioButton
+    lateinit var englishButton : RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        locatioGroup = binding.locationRadioGroup
+        languageGroup = binding.languageRadioGroup
+
+        gpsButton = binding.gpsRadioButton
+        mapButton = binding.mapRadioButton
+        arabicButton = binding.arabicRadioButton
+        englishButton = binding.englishRadioButton
+
+        locatioGroup.setOnCheckedChangeListener{group, checkedId ->
+            if(mapButton.id == checkedId){
+                findNavController().navigate(R.id.action_settingsFragment_to_mapFragment)
+            }
+            else if(gpsButton.id == checkedId){
+
+            }
+        }
+    }
 
 }
