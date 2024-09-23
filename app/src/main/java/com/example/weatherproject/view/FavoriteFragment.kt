4278b1026dbee4e3ddd636184dcddea1598f5fb9
sammar <sammaralaa95@gmail.com>
2024-09-23 07:35:56 +0300

    navigate from favorite to new map

--- app/src/main/java/com/example/weatherproject/view/FavoriteFragment.kt
+++ app/src/main/java/com/example/weatherproject/view/FavoriteFragment.kt
@@ -5,23 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weatherproject.R
+import com.example.weatherproject.databinding.FragmentFavoriteBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class FavoriteFragment : Fragment() {
-
+    lateinit var binding : FragmentFavoriteBinding
+    lateinit var fab : FloatingActionButton
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        binding =  FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab = binding.fab
+        fab.setOnClickListener {
+           findNavController().navigate(R.id.action_favoriteFragment_to_newMapFragment)
+        }
+    }
 }
