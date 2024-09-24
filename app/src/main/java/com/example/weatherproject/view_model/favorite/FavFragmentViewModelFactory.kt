5c784f01d17ef7b2b601ad733476f8d5fad5da00
sammar <sammaralaa95@gmail.com>
2024-09-24 05:00:26 +0300

    implement FavFragmentViewModelFactory

--- app/src/main/java/com/example/weatherproject/view_model/favorite/FavFragmentViewModelFactory.kt
+++ app/src/main/java/com/example/weatherproject/view_model/favorite/FavFragmentViewModelFactory.kt
@@ -1,4 +1,17 @@
 package com.example.weatherproject.view_model.favorite
 
-class FavFragmentViewModelFactory {
-}
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.weatherproject.model.WeatherRepository
+import com.example.weatherproject.view_model.home.HomeFragmentViewModel
+
+class FavFragmentViewModelFactory (private val repo: WeatherRepository) : ViewModelProvider.Factory  {
+
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(FavFragmentViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return FavFragmentViewModel(repo) as T
+            }else
+            {throw IllegalArgumentException("Unknown ViewModel class")}
+        }
+    }
