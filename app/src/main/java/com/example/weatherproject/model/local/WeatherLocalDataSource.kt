c364b4dd776abd4a0faa1425f11f689c409c3d63
sammar <sammaralaa95@gmail.com>
2024-09-24 03:39:03 +0300

    implement localDataSource

--- app/src/main/java/com/example/weatherproject/model/local/WeatherLocalDataSource.kt
+++ app/src/main/java/com/example/weatherproject/model/local/WeatherLocalDataSource.kt
@@ -1,5 +1,21 @@
 package com.example.weatherproject.model.local
 
-class WeatherLocalDataSource {
+import android.util.Log
+import com.example.weatherproject.db.WeatherDao
+import com.example.weatherproject.model.WeatherModel
+import kotlinx.coroutines.flow.Flow
+
+class WeatherLocalDataSource(var dao : WeatherDao) {
+
+    fun getAllWeathers() : Flow<List<WeatherModel>>{
+        return dao.getAll()
+    }
+    fun insertWeather(weatherModel: WeatherModel){
+        var l = dao.insert(weatherModel)
+        Log.i("TAG", "insertWeather: $l from weatherLocalDataSource")
+    }
+    fun deleteWeather(weatherModel: WeatherModel){
+        dao.delete(weatherModel)
+    }
 
 }
