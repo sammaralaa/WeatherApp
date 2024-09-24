290ebe792687a35b7f20fe72c7f4a928cb919330
sammar <sammaralaa95@gmail.com>
2024-09-24 04:09:29 +0300

    implement FavFragmentViewmodel

--- app/src/main/java/com/example/weatherproject/view_model/favorite/FavFragmentViewModel.kt
+++ app/src/main/java/com/example/weatherproject/view_model/favorite/FavFragmentViewModel.kt
@@ -2,15 +2,36 @@ package com.example.weatherproject.view_model.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.weatherproject.model.WeatherModel
 import com.example.weatherproject.model.WeatherRepository
+import kotlinx.coroutines.launch
 
-class FavFragmentViewModel(var repository: WeatherRepository) {
+class FavFragmentViewModel(var repository: WeatherRepository) : ViewModel(){
+
     private var _weather : MutableLiveData<List<WeatherModel>> = MutableLiveData<List<WeatherModel>>()
     val weather  : LiveData<List<WeatherModel>> = _weather
 
+    init {
+        getAllLocalWeather()
+    }
     fun getAllLocalWeather(){
-       // viewModelScope
+        viewModelScope.launch {
+            repository.getLocalWeathers().collect{weathers->
+                _weather.postValue(weathers)
+            }
+        }
+    }
+    fun insertWeather(weatherModel: WeatherModel){
+        viewModelScope.launch {
+            repository.insertWeather(weatherModel)
+        }
+    }
+    fun deleteWeather(weatherModel: WeatherModel){
+        viewModelScope.launch {
+            repository.deleteWeather(weatherModel)
+            getAllLocalWeather()
+        }
     }
 }
