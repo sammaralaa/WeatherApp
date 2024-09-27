a508901e00154777ffb21302cdfc61ef2ad5fa9f
sammar <sammaralaa95@gmail.com>
2024-09-27 04:13:37 +0300

    create screen to display location details from favorites

--- app/src/main/java/com/example/weatherproject/FavDetailsFragment.kt
+++ app/src/main/java/com/example/weatherproject/FavDetailsFragment.kt
@@ -1,32 +1,44 @@
 package com.example.weatherproject
 
+import android.location.Geocoder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.example.weatherproject.databinding.FragmentFavDetailsBinding
+import com.example.weatherproject.databinding.FragmentHomeBinding
+import com.example.weatherproject.view.home.daily_forcast.ForcastItemAdapter
+import com.example.weatherproject.view.home.hourly_forcast.ForcastHourlyAdapter
+import com.example.weatherproject.view_model.favorite.FavFragmentViewModel
+import com.example.weatherproject.view_model.favorite.FavFragmentViewModelFactory
+import com.example.weatherproject.view_model.home.HomeFragmentViewModel
+import com.example.weatherproject.view_model.home.HomeFragmentViewModelFactory
+import com.google.android.gms.location.FusedLocationProviderClient
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FavDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FavDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var favViewModel: FavFragmentViewModel
+    private lateinit var favFactory: FavFragmentViewModelFactory
+    lateinit var desc : TextView
+    private lateinit var binding: FragmentFavDetailsBinding
+
+    lateinit var dailyAdapter : ForcastItemAdapter
+    lateinit var dailyRecyclerView: RecyclerView
+    private lateinit var dailyLayoutManager: LayoutManager
+
+    lateinit var hourlyAdapter : ForcastHourlyAdapter
+    lateinit var hourlyRecyclerView: RecyclerView
+    private lateinit var hourlyLayoutManager: LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -37,23 +49,5 @@ class FavDetailsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_fav_details, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FavDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FavDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
